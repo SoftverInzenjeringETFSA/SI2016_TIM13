40d8a5849524ba8ae9974cb11b5a45af22e76131
esmajlovic <esmajlovic1@etf.unsa.ba>
2017-05-19 23:39:44 +0200

    Nova verzija.

--- app/InstagramLIKE/app/routes/index.js
+++ app/InstagramLIKE/app/routes/index.js
@@ -1,10 +1,8 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-<<<<<<< HEAD
-=======
 	beforeModel(){
 		this.replaceWith('login');
 	}
->>>>>>> 48302f31f8038a1e80adcc0b5b77c73eaf74183a
 });
+
