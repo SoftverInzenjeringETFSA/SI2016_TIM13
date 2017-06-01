ef2368a25180fe670bd9cec0a11d04c1d0035d4f
xdule <Aselmanov@gmail.com>
2017-06-01 16:22:14 +0200

    Create PanelRepository.java

---
+++ app/instagram-backend/src/main/java/instagramlike/repositories/PanelRepository.java
@@ -0,0 +1,23 @@
+package instagramlike.repositories;
+
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import instagramlike.models.Notifikacija;
+import instagramlike.models.Panel;
+
+
+
+@Repository
+public interface PanelRepository extends CrudRepository<Panel, Integer> {
+	
+	public List<Panel> findAll();
+	public Panel findById(Integer ID);
+	public List<Panel> findByKorisnikID(Integer ID);
+	public Boolean deleteById(Integer ID);
+	public Boolean deleteByKorisnikID(Integer ID);
+
+}
